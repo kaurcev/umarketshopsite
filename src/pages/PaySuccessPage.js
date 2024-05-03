bb1eaaaccf1249870ce2b1ee76588a45e267533a
Александр Каурцев <skaurcev2004@yandex.ru>
2024-05-03 08:51:39 +0600

    Update PaySuccessPage.js

--- src/pages/PaySuccessPage.js
+++ src/pages/PaySuccessPage.js
@@ -4,14 +4,13 @@ import E404 from '../img/E404.png'
 import Footer from '../components/Footer';
 
 export default function PaySuccessPage() {
-    document.title = "404 - Страница не найдена";
+    document.title = "Баланс пополнен!";
     useEffect(() => {
         window.scrollTo(0, 0)
     }, [])
     return (
         <>
         <main className='centered'>
-            <img src={E404} alt='404' />
             <h1>Платёж принят!</h1>
             <p>Скоро ваши средства поступят на ваш счёт</p>
             <p><Link className='bt' to='/profile/wallet'>Вернуться в кошелёк</Link></p>
