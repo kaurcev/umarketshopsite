4b2f702ca80aef0d3e779f32e2ecc88d1d704c13
kaurcev <skaurcev2004@yandex.ru>
2024-05-15 20:04:03 +0600

    fixed

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // config.js
-const serverUrl = "api.umarketshop.site";
+export const serverUrl = "api.umarketshop.site";
 // const serverUrl = "backend";
 
 //metrics links default
-const YandexMetrica = "https://metrika.yandex.ru/dashboard?group=day&period=week&id=95864034";
-const AnalyticsGoogle = "https://analytics.google.com/analytics/web/?authuser=0&hl=en#/p419756902/realtime/overview";
+export const YandexMetrica = "https://metrika.yandex.ru/dashboard?group=day&period=week&id=95864034";
+export const AnalyticsGoogle = "https://analytics.google.com/analytics/web/?authuser=0&hl=en#/p419756902/realtime/overview";
