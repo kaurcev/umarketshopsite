3bc4edd800587b1778976b13b5477a7900a99423
kaurcev <skaurcev2004@yandex.ru>
2024-05-20 18:48:42 +0600

    Иконка статуса ответа

--- src/pages/ReplysPage.js
+++ src/pages/ReplysPage.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { serverUrl } from "../config";
 import Header from "../components/Header";
 import Footer from '../components/Footer';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import ModalAlert from '../components/ModalAlert';
 
 export default function ReplysPage() {
-  document.title = "Акция...";
+  document.title = "Панель поставщика | Отзывы";
   const navigate = useNavigate();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -51,23 +51,36 @@ export default function ReplysPage() {
       <>
       <Header />
       <ModalAlert show={showModal} onClose={() => setShowModal(false)} text={modalText} />
-        <main>
-          { loading ? (
+      <main className='profile pay'>
+        <div className='w250'>
+        <Link className='bt' to='/profile/postav'>Вернуться назад</Link>
+        </div>
+        <div className='page'>
+            <h3>ОТЗЫВЫ НА ТОВАРЫ</h3>
+            <p className='mini'>Последние отзывы на ваши товары</p>
+            { loading ? (
           <>
           <p className='noauth'>Идёт загрузка</p>
           </>) : (
           <>
             {
              data.map((item) => (
-              <div key={item.id} className="cart">
+              <div key={item.id} className="cart reply">
                 <h4>{item.username}</h4>
                 <p>{item.message}</p>
+                {
+                  item.reply === "" ? (<>
+                 <p className='statcheck red'><i class="fa fa-times" aria-hidden="true"></i> Требует ответа</p>
+                  </>) : (<>
+                    <p className='statcheck'><i class="fa fa-check" aria-hidden="true"></i> Ответ дан</p>
+                  </>)
+                }
                 <button onClick={() => navigate(`/profile/postav/reply?id=${item.id}`)}>Детали отзыва</button>
               </div>
               ))
           }
           </>)}
-
+        </div>
         </main>
         <Footer />
       </>
