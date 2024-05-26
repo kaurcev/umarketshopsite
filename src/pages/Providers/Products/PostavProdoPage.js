b2938021ade913a1e7b0edbf0081a6e0f23e9fdb
kaurcev <skaurcev2004@yandex.ru>
2024-05-26 19:55:37 +0600

    fixed

--- src/pages/Providers/Products/PostavProdoPage.js
+++ src/pages/Providers/Products/PostavProdoPage.js
@@ -3,6 +3,7 @@ import { useNavigate, Link } from "react-router-dom";
 import Header from "../../../components/Header";
 import { serverUrl } from "../../../config";
 import Footer from "../../../components/Footer";
+import NoAuthPage from "../../NoAuthPage";
 
 export default function PostavProdoPage() {
   document.title = "Панель поставщика";
